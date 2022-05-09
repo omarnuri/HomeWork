08f0c782bdda03c79243f316954f3aabbfd1a9e9
Omar <omarkifanuri@gmail.com>
2022-05-10 01:06:47 +0400

    Теперь точно все!

--- WondersOfTheWorld/WondersOfTheWorld/Program.cs
+++ WondersOfTheWorld/WondersOfTheWorld/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+namespace WondersOfTheWorld
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Wonders.Wonder_1 omar = new Wonders.Wonder_1();
+            omar.Name();
+        }
+    }
+}
