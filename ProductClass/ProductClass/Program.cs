b914bbb5a958a8a8b110252a9c07161284610c77
Omar <omarkifanuri@gmail.com>
2022-05-10 22:59:41 +0400

    Сделал дз с enum и структурами

---
+++ ProductClass/ProductClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Product
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Student student = new();
+            student.Name = "Omar";
+            student.Surname = "Nuriyev";
+            student.Patronymic = "Ramin";
+            student.Age = 15;
+            student.AddGrade(Subject.Programming, 1);
+            student.AddGrade(Subject.Programming, 12);
+            student.Print();
+
+
+
+
+        }
+    }
+}
---
+++ ProductClass/ProductClass/Shop.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace Shop
+{
+    struct Article
+    {
+        public Article() {
+            {
+                _Id++;
+                Id = _Id;
+                Name = String.Empty;
+                Value = 0;
+                Type = null;
+            }
+        }
+        private static int _Id { get; set; } = 0;
+        public  int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public ArticleTypes? Type { get; set; }
+    }
+    struct Client
+    {
+        public string Name{ get; set; }
+        public string Surname{ get; set; }
+        public string Patronymic { get; set; }
+        public string Address { get; set; }
+        public long PhoneNumber { get; set; }
+        public int CountOfAllOrders { get; set; }
+        public decimal CostOfAllOrders { get; set; }
+        public ClientTypes? Type { get; set; }
+
+    }
+    struct RequestItem
+    {
+        public Article Item { get; set; }
+        public int CountOfItem { get; set; }
+        public string ToString(RequestItem item)
+        {
+            if (Item.Name != null) {
+                return $"Item Name:{Item} Count of item:{CountOfItem}";
+            }
+            else { return "NULL"; }
+        }
+
+    }
+    struct Request
+    {
+       public Request()
+        {
+            Id++;
+            OrderId = Id;
+            Client = new();
+            RequestDate = DateTime.Now;
+            OrdersCost = 0;
+            _AllReguestedItmes = null;
+            Type = null;
+        }
+        private static int Id { get; set; } = 0;
+        public static int OrderId { get; set; }
+        public Client Client { get; set; }
+        public DateTime RequestDate { get; set; }
+        public decimal OrdersCost { get; private  set; }
+        private RequestItem[]? _AllReguestedItmes;
+        public RequestItem[] AllReguestedItmes 
+        {
+            get
+            {
+                if(this._AllReguestedItmes == null)
+                {
+                    Console.WriteLine("Вы забыли инициализзировать переменную и хотели получить ее значение :( ");
+                    throw new ArgumentNullException();
+                }
+                return this._AllReguestedItmes;
+            }
+            set
+            {
+                _AllReguestedItmes = value;
+                if (_AllReguestedItmes != null)
+                {
+                    foreach (RequestItem item in _AllReguestedItmes)
+                    {
+                        OrdersCost += item.Item.Value;
+                    }
+                }
+            }
+        }
+        PayTypes? Type { get; set; }
+
+    }
+    enum ArticleTypes
+    {
+        ConsumersGoods,
+        CapitalGoods,
+        DigitalGoods,
+        VirtualGoods,
+        VeblenGoods,
+        Commodities,
+        UnsoughtGoods,
+        ComplementaryGoods,
+        SuperiorGoods,
+        ConsumerDiscretionary,
+        SubsituteGoods,
+        DurableGoods,
+        SearchGoods,
+        ExpirienceGoods,
+        PerfectSubstitutes,
+        FashionGoods,
+        NecessityGoods,
+        FastMovingConsumersGoods,
+        MeritGoods,
+        KnowledgeProducts,
+        IntangibleGoods,
+        FinishedGoods,
+        InferiorGoods,
+        PublicGoods,
+        CommonResources,
+        ConvenienceGoods,
+    }
+    enum ClientTypes
+    {
+        ClientWithPersonalConnections,
+        RichClient,
+        EfficientClient,
+        UrgentClient,
+        ClientWithNegativeExperience,
+        ClientWithPositiveExperience,
+        TrendyClients,
+        ClientConcernedAboutSecurity,
+        ClientWhoWorkOnCommittees,
+        IndecisiveClients
+
+    }
+    enum PayTypes
+    {
+        InCash,
+        PaymentSystem
+
+    }
+}
