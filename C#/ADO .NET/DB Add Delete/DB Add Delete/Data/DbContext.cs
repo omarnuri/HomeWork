3234ec1d433c82169cbcf54e7445b935496656c4
Omar Nuriyev <omarkifanuri@gmail.com>
2023-06-04 15:49:46 +0300

    ADO .NET

---
+++ C#/ADO .NET/DB Add Delete/DB Add Delete/Data/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Animal> Animals { get; set; }
+
+    public AppDbContext() : base("data source=as;initial catalog=Zoo;trusted_connection=true")
+    {
+        
+    }
+}
+
---
+++ C#/ADO .NET/DB Add Delete/DB Add Delete/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DB_Add_Delete
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void ShowDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new AppDbContext())
+            {
+                int animalCount = int.Parse(AnimalCountTextBox.Text);
+
+                var animalsQuery = db.Animals.Take(animalCount);
+                var animals = animalsQuery.ToList();
+
+                DataGridAnimals.ItemsSource = animals;
+            }
+        }
+
+        private void FilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new AppDbContext())
+            {
+                string species = SpeciesTextBoxFind.Text;
+
+                var animalsQuery = db.Animals.Where(a => a.Species == species);
+                var animals = animalsQuery.ToList();
+
+                DataGridAnimals.ItemsSource = animals;
+            }
+        }
+
+
+     
+
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new AppDbContext())
+            {
+                string name = NameTextBoxAdd.Text;
+                string species = SpeciesTextBoxAdd.Text;
+                int age = int.Parse(AgeTextBoxAdd.Text);
+
+                var newAnimal = new Animal { Name = name, Species = species, Age = age };
+
+                db.Animals.Add(newAnimal);
+                db.SaveChanges();
+
+                
+                ShowDataButton_Click(sender, e);
+            }
+        }
+
+
+
+
+
+    }
+}
